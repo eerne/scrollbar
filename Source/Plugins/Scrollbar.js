01a0ae16c8ffb03217228c8b9da564e08b9d613c
Enrique Erne <enrique.erne@gmail.com>
2009-07-11 18:22:20 +0200

    automatic steps calculation, code optimization

--- Source/Plugins/Scrollbar.js
+++ Source/Plugins/Scrollbar.js
@@ -10,38 +10,47 @@ Copyright:
 
 */
 var ScrollBar = new Class({
+
 	Extends: Slider,
+
 	options: {
 		slideable: {}
 	},
+
 	initialize: function(slideable, slider, knob, options){
 		this.knob = knob;
 		this.parent(slider, knob, options);
+		this.innerSlideable = slideable.getFirst();
+		this.steps = this.innerSlideable.getSize()[this.axis] - slideable.getSize()[this.axis];
 		this.scroll = new Fx.Scroll(slideable, options.slideable);
 		this.addEvent('complete', function(event){
 			if (event.target != knob) this.move();
 		});
-		// this.ratio = ((2000-802)/657); // ScrollableWidth, Container, SliderWidth-KnobWidth
-		this.ratio = ((slideable.getFirst().getSize()[this.axis] - slideable.getSize()[this.axis]) / (slider.getSize()[this.axis] - knob.getSize()[this.axis]));
+		this.ratio = this.steps / (slider.getSize()[this.axis] - knob.getSize()[this.axis]);
 	},
+
 	set: function(step){
-		if($type(step) === 'element') step = step.getPosition(step.getParent())[this.axis] / this.ratio;
-		this.knob.tween((this.options.mode === 'vertical') ? 'top' : 'left', step);
+		if($type(step) === 'element') step = step.getPosition(this.innerSlideable)[this.axis] / this.ratio;
+		this.knob.tween(this.property, step);
 		this.move(step * this.ratio);
 	},
+
 	move: function(step){
 		if (this.options.mode === 'vertical') this.scroll.cancel().start(0, step ? step : this.step);
 		else this.scroll.cancel().start(step ? step : this.step);
 	},
+
 	draggedKnob: function(){
 		this.parent();
 		if (this.options.mode === 'vertical') this.scroll.cancel().set(0, this.step);
 		else this.scroll.cancel().set(this.step);
 	},
+
 	clickedElement: function(event){
 		if (event.target == this.knob) return;
 		var position = event.page[this.axis] - this.element.getPosition()[this.axis] - this.half;
 		position = position.limit(-this.options.offset, this.full -this.options.offset);
 		this.set(position);
 	}
+
 });
