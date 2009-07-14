23f997726c62b30cb8451c73935c1818ace7e40b
Enrique Erne <enrique.erne@gmail.com>
2009-07-14 22:32:02 +0200

    added $chk to fix set(0) bug part.II

--- Source/Plugins/Scrollbar.js
+++ Source/Plugins/Scrollbar.js
@@ -9,29 +9,25 @@ Copyright:
 	Copyright (c) 2009 Enrique Erne (http://mild.ch).
 
 */
-var ScrollBar = new Class({
 
+
+	
+var ScrollBar = new Class({
+	
 	Extends: Slider,
 
 	options: {
 		fx: {}
 	},
 
-	initialize: function(scroller, slider, knob, options){
-		switch (this.options.mode){
-			case 'vertical':
-				this.axis = 'y';
-				this.property = 'top';
-				break;
-			case 'horizontal':
-				this.axis = 'x';
-				this.property = 'left';
-		}
+	initialize: function(scroller, slider, knob, options){/*
+		onChange: $empty(intStep),
+		onComplete: $empty(strStep),*/
 		this.knob = document.id(knob).set('tween', options.fx);
 		this.slider = document.id(slider);
-		this.parent(this.slider, this.knob, options);
 		this.scroller = document.id(scroller);
 		this.scrollElement = this.scroller.getFirst();
+		this.parent(this.slider, this.knob, options);
 		this.steps = this.scrollElement.getSize()[this.axis] - this.scroller.getSize()[this.axis];
 		this.scroll = new Fx.Scroll(this.scroller, options.fx);
 		/*this.addEvent('complete', function(event){
@@ -44,12 +40,13 @@ var ScrollBar = new Class({
 		if($type(position) === 'element') position = position.getPosition(this.scrollElement)[this.axis] / this.ratio;
 		position = position.limit(-this.options.offset, this.full -this.options.offset);
 		this.move(position * this.ratio);
-		this.knob.tween(this.property, position);
+		this.knob.tween(this.property, position)
+		// this.fireEvent('complete', this.step + '');
 	},
 
-	move: function(step){
-		if (this.options.mode === 'vertical') this.scroll.cancel().start(0, step ? step : this.step);
-		else this.scroll.cancel().start(step ? step : this.step);
+	move: function(position){
+		if (this.options.mode === 'vertical') this.scroll.cancel().start(0, $chk(position) ? position : this.step);
+		else this.scroll.cancel().start($chk(position) ? position : this.step);
 	},
 
 	draggedKnob: function(){
@@ -59,7 +56,7 @@ var ScrollBar = new Class({
 	},
 
 	clickedElement: function(event){
-		if (event.target == this.knob) {
+		if (event.target === this.knob) {
 			this.knob.get('tween').cancel();
 			return;
 		}
